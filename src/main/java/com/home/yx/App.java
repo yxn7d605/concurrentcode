fe894c0e268f932b9cd0212aa83b150595f87977
yxn7d605 <yxn7d605@126.com>
2020-08-06 17:19:51 +0800

    add

--- src/main/java/com/home/yx/App.java
+++ src/main/java/com/home/yx/App.java
@@ -5,6 +5,12 @@ package com.home.yx;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(Integer.SIZE);
+
+        int r = -1 << 29;
+        System.out.println(r);
+
+        int CAPACITY   = (1 << 3) - 1;
+        System.out.println(CAPACITY);
     }
 }
